24bbe144432a0c5f55855dc265de24c452c8b404
ilham444 <2206020@itg.ac.id>
2025-05-29 01:31:18 +0700

    Update

--- lib/routes/app_route.dart
+++ lib/routes/app_route.dart
@@ -1,6 +1,5 @@
 import 'package:bbc_news/views/home_screen.dart';
 import 'package:bbc_news/views/login_page.dart';
-import 'package:bbc_news/views/profil_screen.dart';
 import 'package:bbc_news/views/register_page.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -33,9 +32,7 @@ class AppRouter {
         GoRoute(
           path: '/home',
           name: RouteNames.home,
-          pageBuilder:
-              (context, state) =>
-                  MaterialPage(child: ProfilePage()), //dirubahhhhhhhh
+          pageBuilder: (context, state) => MaterialPage(child: HomeScreen()),
         ),
         GoRoute(
           path: '/login',
