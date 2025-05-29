de72345291a2b64591a4c3688e0ea0184046d9eb
MuhAdhit63 <2206032@itg.ac.id>
2025-05-29 21:11:09 +0700

    perbaikan navigasi kategori

--- lib/widgets/category_button.dart
+++ lib/widgets/category_button.dart
@@ -1,4 +1,9 @@
 import 'package:bbc_news/utils/helper.dart';
+import 'package:bbc_news/views/all_categories_screen.dart';
+import 'package:bbc_news/views/criminal_screen.dart';
+import 'package:bbc_news/views/hiburan_screen.dart';
+import 'package:bbc_news/views/politic_screen.dart';
+import 'package:bbc_news/views/sport_screen.dart';
 import 'package:flutter/material.dart';
 import '../views/detail_page.dart';
 
@@ -22,10 +27,46 @@ class CategoryButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => DetailPage(pageTitle: navigationPageName),)
-        );
+        if (navigationPageName == "politics"){
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => PoliticScreen()
+            )
+          );
+        } else if (navigationPageName == "entertainment") {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => HiburanScreen()
+            )
+          );
+        } else if (navigationPageName == "sports") {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => SportScreen()
+            )
+          );
+        } else if (navigationPageName == "criminal") {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => CriminalScreen()
+            )
+          );
+        } else if (navigationPageName == "all") {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AllCategoriesScreen()
+            )
+          );
+        }
+        // Navigator.push(
+        //   context,
+        //   MaterialPageRoute(builder: (context) => DetailPage(pageTitle: navigationPageName),)
+        // );
       },
       child: Column(
         mainAxisSize: MainAxisSize.min,
